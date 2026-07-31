64f814943604f705d0f3cedf0cd3776c67e2caca
lthnh <imr3ng4l@gmail.com>
2026-07-31 23:53:18 +0700

    modify project build script and compile args to make it compilable

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir -p configs/generated
-mkdir -p ../ccs_workspace
+mkdir -p /home/thomas/workspace_ccstheia
 mkdir -p artifact
 touch ./configs/generated/autoconf.h
-/opt/ti/ccs1020/ccs/eclipse/eclipse -noSplash -data ../ccs_workspace -application "com.ti.ccstudio.apps.projectImport" -ccs.location .
+/home/thomas/ti/ccs2100/ccs/eclipse/ccs-server-cli.sh -noSplash -workspace '/home/thomas/workspace_ccstheia' -application 'projectImport' -ccs.location .
 
 configNames=$(find configs/ -type f -name "*_defconfig")
 for configName in ${configNames}
@@ -14,7 +14,7 @@ do
     echo "Building for $name"
     echo "***********************************************************"
     make defconfig CONF=$name
-    /opt/ti/ccs1020/ccs/eclipse/eclipse -noSplash -data ../ccs_workspace -application "com.ti.ccstudio.apps.projectBuild" -ccs.projects "CANopenNode_bootloader"
+    /home/thomas/ti/ccs2100/ccs/eclipse/ccs-server-cli.sh -noSplash -workspace '/home/thomas/workspace_ccstheia' -application 'projectBuild' -ccs.projects 'CANopenNode_bootloader' -ccs.autoImport
     mkdir -p artifact/$name
     cp Debug/CANopenNode_bootloader.* artifact/$name
     cp configs/$name artifact/$name/$name
