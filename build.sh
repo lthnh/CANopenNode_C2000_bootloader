054b23fd654570774f0d4247e552b532f9367581
lthnh <imr3ng4l@gmail.com>
2026-08-04 00:20:42 +0700

    fix build script again

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir -p configs/generated
-mkdir -p /home/thomas/workspace_ccstheia
+mkdir -p ../../workspace_ccstheia
 mkdir -p artifact
 touch ./configs/generated/autoconf.h
-/home/thomas/ti/ccs2100/ccs/eclipse/ccs-server-cli.sh -noSplash -workspace '/home/thomas/workspace_ccstheia' -application 'projectImport' -ccs.location .
+ccs-server-cli.sh -noSplash -workspace ../../workspace_ccstheia -application projectImport -ccs.location .
 
 configNames=$(find configs/ -type f -name "*_defconfig")
 for configName in ${configNames}
@@ -14,7 +14,7 @@ do
     echo "Building for $name"
     echo "***********************************************************"
     make defconfig CONF=$name
-    /home/thomas/ti/ccs2100/ccs/eclipse/ccs-server-cli.sh -noSplash -workspace '/home/thomas/workspace_ccstheia' -application 'projectBuild' -ccs.projects 'CANopenNode_bootloader' -ccs.autoImport
+    ccs-server-cli.sh -noSplash -workspace ../../workspace_ccstheia -application projectBuild -ccs.projects CANopenNode_bootloader -ccs.autoImport
     mkdir -p artifact/$name
     cp Debug/CANopenNode_bootloader.* artifact/$name
     cp configs/$name artifact/$name/$name
